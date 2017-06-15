2776d03cbf4234e34d5ac2141eeabbb22f7d7cfe
mianhk <yugc666@163.com>
2017-06-15 22:03:08 +0800

    类

---
+++ ch7/deal_exange.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include "Scales_data.h"
+using namespace std;
+
+int main(){
+	cout<<"请输入交易记录（ISBN、销售量、原价、实际售价）："<<endl;
+	Sales_data total;
+	if(read(cin,total)){
+		Sales_data trans;
+		while(read(cin,trans)){
+			if(total.isbn()==trans.isbn())
+				total.combine(trans);
+			else{
+				print(cout,total)<<endl;
+				total=trans;
+			}
+		}
+		print(cout,total)<<endl;
+	}
+	else{
+		cerr<<"No data?"<<endl;
+		return -1;
+	}
+	
+	return 0;
+}
+
---
+++ ch7/test1.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main(){
+	for(unsigned i=10;i>=0;--i){
+		cout<<i<<endl;
+	}
+}
